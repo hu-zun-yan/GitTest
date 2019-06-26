4094480d179c8519099f290971ecc559ad607520
hu-zun-yan <40624598+hu-zun-yan@users.noreply.github.com>
2019-06-26 13:31:07 +0800

    Add files via upload

---
+++ index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="back-arrow" @tap="handleClick"></div>
+</template>
+
+<script>
+export default {
+  name: 'Back',
+  props: {
+    root: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$router.pop(this.root);
+      this.$emit('click');
+    }
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.back-arrow {
+  width: 18px;
+  height: 33px;
+  background-image: url(./back-arrow@2x.png);
+  background-size: 100% 100%;
+  position: relative;
+  &:after {
+    position: absolute;
+    content: '';
+    top: -26px;
+    bottom: -26px;
+    left: -32px;
+    right: -32px;
+  }
+}
+</style>
